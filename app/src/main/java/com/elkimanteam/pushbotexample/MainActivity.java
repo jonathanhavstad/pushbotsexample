7e2024b1d808d71f87a6a0fcb1efb7e1e07d9608
Jonathan Havstad <jjhavstad@gmail.com>
2016-10-17 16:53:32 -0700

    First commit

---
+++ app/src/main/java/com/elkimanteam/pushbotexample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.elkimanteam.pushbotexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pushbots.push.Pushbots;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Pushbots.sharedInstance().init(this);
+    }
+}
